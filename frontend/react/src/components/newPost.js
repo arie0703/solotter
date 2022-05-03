596e1b486a36419ea307abf555f3939d5aa52af5
arie0703 <[ari0703sophia@gmail.com]>
2022-05-04 07:38:02 +0900

    削除機能

--- frontend/react/src/components/newPost.js
+++ frontend/react/src/components/newPost.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import '../index.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 class NewPost extends Component {
     constructor(props) {
         super(props)
@@ -36,13 +37,15 @@ class NewPost extends Component {
     render() {
         return (
         <div className="newPost">
+            
             <form onSubmit={this.handleSubmit.bind(this)} class="post_form">
+            <Box sx={{display: "flex", margin: "0 auto", justifyContent: "center", width: "80%"}}>
             <TextField
             name="body" 
             InputProps={{
               style: {color: 'white'}
             }}
-            sx={{width: "40%",borderColor: 'white'}}
+            sx={{width: "40%",outlineColor: '#fff', }}
             type="text"
             InputLabelProps={{ style: {color: 'white'}}}
             label="body" 
@@ -52,8 +55,10 @@ class NewPost extends Component {
             fullWidth
             /><br/>
 
-            <Button type="submit">Tweet</Button>
+            <Button type="submit" variant="contained" sx={{height: "50px", margin:"10px 4px", backgroundColor: "#1DA1F2"}}>Tweet</Button>
+            </Box>
             </form>
+            
         </div>
         );
     }
